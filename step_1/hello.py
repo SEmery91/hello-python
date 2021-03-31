6fc065b2043afbb4b135fa03c64886ab6b7f3f5c
Sean Emery <s.emery91@gmail.com>
2021-03-31 20:56:13 +1100

    Adding basic examples

---
+++ step_1/hello.py
@@ -0,0 +1,4 @@
+x = 5
+y = "hello python"
+
+print(y)
---
+++ step_2/hello.py
@@ -0,0 +1,6 @@
+def format_position(lat, long):
+    formatedPosition = f"Lat: {lat} - Long: {long}"
+    return formatedPosition
+
+
+print(format_position(-15.23, 120.00))
